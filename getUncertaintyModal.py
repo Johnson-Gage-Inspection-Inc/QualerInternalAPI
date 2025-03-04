6b7790aa72ada1a9e6e5abc97a8303ea58d1927c
Jeff Hall <rhythmatician5@gmail.com>
2025-03-04 16:30:14 -0600

    Add script to fetch and store uncertainty parameters from Qualer API

---
+++ getUncertaintyModal.py
@@ -0,0 +1,22 @@
+from my_qualer_utils import QualerAPIFetcher
+from tqdm import tqdm
+
+with QualerAPIFetcher() as api:
+    query = """SELECT
+        m.measurementid,
+        ms.batchid
+    FROM measurements m
+    JOIN measurement_points mp ON m.measurementid = mp.measurementid
+    JOIN measurement_sets ms ON mp.measurementsetid = ms.measurementsetid
+    LIMIT 10;
+"""
+    queryResult = api.run_sql(query)
+    for (measurementID, batchId) in tqdm(queryResult):
+        url = (
+            "https://jgiquality.qualer.com/work/Uncertainties/UncertaintyParameters?"
+            f"measurementId={measurementID}&measurementBatchId={batchId}"
+        )
+        try:
+            api.fetch_and_store(url, "UncertaintyParameters")
+        except Exception as e:
+            print(f"Error fetching {url}: {e}")
