b30dc1df5f8a0655211ab62790af09f4b9961acb
Jeff Hall <rhythmatician5@gmail.com>
2025-03-05 12:52:17 -0600

    Add ToolTypes.json and parser script for database integration

---
+++ parseToolTypes.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import json
+
+# Connect to your Postgres DB (adjust connection string as needed)
+engine = create_engine("postgresql://postgres:postgres@192.168.1.177:5432/qualer")
+
+file = "C:/Users/JGI/Jeff H/Escape Dantes Inferno/QualerInternalAPI/ToolTypes.json"
+with open(file, "r") as f:
+    data = json.load(f)
+
+df = pd.DataFrame(data)
+df.to_sql("tool_types", engine, if_exists="append", index=False)
+
+# # Then, run this SQL:
+# ALTER TABLE IF EXISTS public.tool_types DROP COLUMN IF EXISTS "Selected";
+
+# ALTER TABLE IF EXISTS public.tool_types
+#     ALTER COLUMN "Text" SET NOT NULL;
+
+# ALTER TABLE IF EXISTS public.tool_types
+#     ALTER COLUMN "Value" SET NOT NULL;
+# ALTER TABLE IF EXISTS public.tool_types
+#     ADD PRIMARY KEY ("Value");
+
+# ALTER TABLE IF EXISTS public.tool_types
+#     RENAME "Text" TO name;
+
+# ALTER TABLE IF EXISTS public.tool_types
+#     RENAME "Value" TO id;
