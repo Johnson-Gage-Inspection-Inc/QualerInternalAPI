918764a721cf878990621d5667cfe03ae4186d89
Jeff Hall <rhythmatician5@gmail.com>
2026-01-07 16:03:15 -0600

    refactor: Move FilterType, SortField, and SortOrder enums to types.py

--- qualer_internal_sdk/endpoints/client_dashboard/clients_read.py
+++ qualer_internal_sdk/endpoints/client_dashboard/clients_read.py
@@ -3,39 +3,7 @@
 from time import sleep
 
 from utils.auth import QualerAPIFetcher
-from enum import Enum
-
-
-class FilterType(str, Enum):
-    """Enumeration of filter types for Clients_Read endpoint."""
-
-    AllClients = "AllClients"
-    Prospects = "Prospects"
-    Delinquent = "Delinquent"
-    Inactive = "Inactive"
-    Unapproved = "Unapproved"
-    Hidden = "Hidden"
-    # Managed assets:
-    AssetsDue = "AssetsDue"
-    AssetsPastDue = "AssetsPastDue"
-
-
-class SortField(str, Enum):
-    """Fields available for sorting clients."""
-
-    ClientCompanyName = "ClientCompanyName"
-    ClientAccountNumber = "ClientAccountNumber"
-    ContactName = "ContactName"
-    CreatedDate = "CreatedDate"
-    AssetCount = "AssetCount"
-    OrdersCount = "OrdersCount"
-
-
-class SortOrder(str, Enum):
-    """Sort direction."""
-
-    Ascending = "asc"
-    Descending = "desc"
+from .types import FilterType, SortField, SortOrder
 
 
 def clients_read(
